1ebfdf4433b3a2c8eb71518d90abd637aaaf2d5f
dench111 <46183310+dench111@users.noreply.github.com>
2019-12-04 16:28:25 +0300

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -8,7 +8,7 @@ pipeline {
     stage("Start Ansible Playbook from Git") {
       steps {
         script {
-          def playbookName = AnsiblePlaybook.yml
+          def playbookName = "AnsiblePlaybook.yml"
           def workspace = WORKSPACE
           sh "chmod ugo+rwx WORKSPACE"
           sh "ansible-playbook -i " + "inventory" + " " + workspace + "/" + playbookName
