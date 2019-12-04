b66909cc28e096f61ab2eb93b8a51e8bb13a239e
dench111 <46183310+dench111@users.noreply.github.com>
2019-12-04 16:33:23 +0300

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -10,7 +10,7 @@ pipeline {
         script {
           def playbookName = "AnsiblePlaybook.yml"
           def workspace = "/var/lib/jenkins/workspace/Ansible_Git"
-          sh "chmod ugo+rwx $workspace"
+          sh "chmod ugo+rwx $workspace/*"
           sh "ansible-playbook -i " + "inventory" + " " + $workspace + "/" + $playbookName
         }
       }
