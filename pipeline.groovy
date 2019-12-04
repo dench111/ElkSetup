3299ef01a40d62c42768f891685fedb9e2a7fdc8
dench111 <46183310+dench111@users.noreply.github.com>
2019-12-04 14:05:21 +0300

    Create pipeline.groovy

---
+++ pipeline.groovy
@@ -0,0 +1,19 @@
+def playbookName = "AnsiblePlaybook.yml"
+pipeline {
+  agent {
+    node {
+      label " "
+    }
+  }
+  stages {
+    stage("Start Ansible Playbook from Git") {
+      steps {
+        script {
+          def workspace = "/var/lib/jenkins/workspace/Ansible_Git"
+          sh "chmod ugo+rwx $workspace/*"
+          sh "ansible-playbook $workspace + "/" + $playbookName"
+        }
+      }
+    }
+  }
+}
