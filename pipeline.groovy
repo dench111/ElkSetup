603ce95e08421245db2cc1d056fe6e70cddbf00b
dench111 <46183310+dench111@users.noreply.github.com>
2019-12-06 16:07:47 +0300

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -13,16 +13,16 @@ pipeline {
           sh "ansible -m setup servers > $workspace/Ansible_env_variables.txt"
           sh "ansible-playbook -i " + "inventory" + " " + "$workspace/Elastic-install-pb.yml"
       }
+     }
+    }
     stage("Congigurating elasticsearch nodes") {
       steps {
         script {
           def workspace = "/var/lib/jenkins/workspace/Ansible_Git"
           sh "chmod ugo+rwx $workspace/*"
           sh "ansible-playbook -i " + "inventory" + " " + "$workspace/Elastic_Nodes-conf-pb.yml"
-        }        
+       }        
      }
     }
    }
-  }
- }
 }
